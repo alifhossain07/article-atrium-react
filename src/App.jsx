bb694763700feb161557fede0dafac0ba29c348a
Alif Hossain <alifhossain56782@gmail.com>
2024-08-13 23:33:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
+
+function App() {
+  
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
