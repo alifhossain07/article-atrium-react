3888d850ba99da9a811887d391e3de48123a41ea
Alif Hossain <alifhossain56782@gmail.com>
2024-08-15 02:40:41 +0600

    Base Project Done (responsive

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -6,7 +6,7 @@ const Bookmarks = ({bookmarks,readingTime}) => {
   return (
     <div className="md:w-1/3 lg:w-1/3 lg:ml-20 md:ml-20 mt-20 border-l-4 border-sky-500  ">
             <div className='ml-10 text-2xl font-semibold border border-sky-400 px-8 py-10 rounded-xl mb-10'>
-                <h3>Total Reading Time :  {readingTime} </h3>
+                <h3>Total Reading Time :  {readingTime} min</h3>
             </div>
 
             <h2 className="text-2xl font-semibold ml-5">Bookmarked Articles: {bookmarks.length}</h2>
